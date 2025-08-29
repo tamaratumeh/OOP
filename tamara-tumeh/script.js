4dbdf59d928765ad0e12abe0951e4f525b4a1b18
Tamara <tamaratumeh5@gmail.com>
2025-08-29 18:53:31 +0300

    oop

--- tamara-tumeh/script.js
+++ tamara-tumeh/script.js
@@ -1,26 +1,27 @@
-function Student(name, age, grade) {
+function Student(name, age, introduce) {
     this.name = name;
     this.age = age;
-    let Grade = grade; 
+    let grade; 
 
     this.getGrade = function () {
-        return Grade;
+        return grade;
     };
 
     this.setGrade = function (value) {
         if (value === "A" || value === "F") {
-            Grade = value;
+            grade = value;
         } else {
             console.log("Invalid grade Only 'A' or 'F' are allowed.");
         }
     };
 }
+
  Student.prototype.introduce = function () {
-        console.log(`Hi, my name is ${this.name}, I’m ${this.age} years old, and I’m in grade ${Grade}`);
+        console.log(`Hi, my name is ${this.name}, I’m ${this.age} years old, and I’m in grade ${this.getGradegrade}`);
     };
 
-const student1 = new Student("tamara", 21, "A")
+const student1 = new Student("tamara", 21, "A");
 student1.introduce()
-student1.setGrade("F")
+student1.setGrade("F"); 
 console.log(student1.getGrade())
 student1.setGrade('B')
